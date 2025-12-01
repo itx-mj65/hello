b86c36283b493f5382fdf03753a8fb7d8c48c861
itx-mj65 <jamshedweb65@gmail.com>
2025-12-01 13:35:41 +0500

    1 of dec

---
+++ main.js
@@ -0,0 +1,17 @@
+let a = 40
+
+if (a > 10) {
+    if (a < 30) {
+        console.log("your number is greater than 10 and less then 30")
+    }
+}
+
+
+//nested loop
+num = 10
+
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        console.log(i, j)
+    }
+}
